b072cd6589fdf4cea4429b51bd86795c57444923
Marssis <mcarvalhodash@gmail.com>
2020-03-23 16:17:47 -0300

    Separating issued commands and parameters

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ const bot = new Discord.Client()
 
 const TOKEN = process.env.TOKEN
 
+bot.on('message', msg => {
+    if (msg.mentions.has(process.env.BOT_CLIENT_ID) ) {
+        let call = msg.content.match(`<@!${process.env.BOT_CLIENT_ID}>(.*)`)[1].trim().split(/\s+/)
+        let command = call.shift()
+        let param = call.join(" ")
+        console.info(`${command}, ${param}`)
+    }
+})
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`)
+})
+
 bot.login(TOKEN)
+
+process.on('exit', () => {
+    bot.destroy()
+})
